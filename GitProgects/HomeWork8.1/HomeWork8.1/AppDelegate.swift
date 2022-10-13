2c8c80626b97715b3b4fb6523ad5d0a073322867
alena.ferents <let4itsa@gmail.com>
2022-10-13 09:51:58 +0300

    LSN - 8. Project created

---
+++ GitProgects/HomeWork8.1/HomeWork8.1/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  HomeWork8.1
+//
+//  Created by Ференц Алена on 12.10.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+
+}
+
---
+++ GitProgects/HomeWork8.1/HomeWork8.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork8.1
+//
+//  Created by Ференц Алена on 12.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello")
+    }
+
+
+}
+
